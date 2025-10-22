7363f442b3a129117cbd29c83c52be67270fdb4e
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-10-22 18:46:00 -0300

    first commit

---
+++ src/main/java/com/example/supergestor/SupergestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.supergestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupergestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupergestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/supergestor/SupergestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supergestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupergestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
