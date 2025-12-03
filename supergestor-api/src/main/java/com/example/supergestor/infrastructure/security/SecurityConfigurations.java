2f168462db28b2a0e6629d5eca2a243fbc73a1b4
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-12-03 14:01:28 -0300

    build: adicionar dockerfile do backend, docker-compose da aplicação e config do prometheus

--- supergestor-api/src/main/java/com/example/supergestor/infrastructure/security/SecurityConfigurations.java
+++ supergestor-api/src/main/java/com/example/supergestor/infrastructure/security/SecurityConfigurations.java
@@ -41,9 +41,9 @@ public class SecurityConfigurations {
                         // Swagger
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
 
-                        // Health público
+                        // Actuator + Prometheus
                         .requestMatchers("/actuator/health").permitAll()
-
+                        .requestMatchers("/actuator/prometheus").permitAll()
                         .requestMatchers("/actuator/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
