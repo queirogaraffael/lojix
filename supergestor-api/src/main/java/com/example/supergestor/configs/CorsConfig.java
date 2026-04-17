e17e4458116d6dd859e4dd349c758202f1769663
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2026-04-17 19:00:21 -0300

    refactor: melhiras no docker compose e no nginx.conf I

--- supergestor-api/src/main/java/com/example/supergestor/configs/CorsConfig.java
+++ supergestor-api/src/main/java/com/example/supergestor/configs/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins:http://localhost}")
+    @Value("${cors.allowed-origins}")
     private String allowedOrigins;
 
     @Bean
@@ -18,7 +18,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins.split(","))
+                        .allowedOriginPatterns(allowedOrigins.split(","))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
